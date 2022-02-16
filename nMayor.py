945749e1a593ce5a9879b2c22f9d5a0ba55cd022
Ommaciasd <79873527+Ommaciasd@users.noreply.github.com>
2022-02-16 02:36:21 -0500

    Add files via upload

---
+++ nMayor.py
@@ -0,0 +1,11 @@
+myArray = [13,2,4,35,1]
+tArreglo = len(myArray)
+nMayor=max(myArray) # IMPRIME EL NUMERO MAYOR DEL ARREGLO.
+
+# INDICAR RANGO ENTRE 1 Y 100.
+if tArreglo >0 or tArreglo ==100:
+
+    print(nMayor) # VALIDAR QUE NO SE IMPRIMA DOS VECES.
+    
+else:
+    print('Por favor establezca un nuevo arreglo con un numero en la lista que no supere los cien elementos.')
